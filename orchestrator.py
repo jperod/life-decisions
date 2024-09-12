edd0e8ba8155ab57f5e40d41775811f5b74930dd
Pedro <pedro@bubbaleads.com>
2024-09-12 21:14:18 +0200

    Auto Update README.md with latest information

---
+++ orchestrator.py
@@ -0,0 +1,19 @@
+import schedule
+import time
+import subprocess
+import datetime
+import os
+import sys
+
+def job():
+    venv_python_path = os.path.join(os.path.dirname(sys.executable), 'python')
+    script_path = r"C:\Repos\life-decisions\jacket\jacket.py"
+    subprocess.run([venv_python_path, script_path])
+
+# Schedule the job every 20 minutes
+schedule.every(10).seconds.do(job)
+
+while True:
+    print(datetime.datetime.now())
+    schedule.run_pending()
+    time.sleep(1)  # Sleep for 1 second to prevent high CPU usage
