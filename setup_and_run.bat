5b276c5f74ff0eab3fe9eb8aaaf03a1a730aaee3
Pedro Rodrigues <p3dror@gmail.com>
2024-09-13 08:19:52 +0200

    add pythonj vers

--- setup_and_run.bat
+++ setup_and_run.bat
@@ -3,13 +3,14 @@ SETLOCAL ENABLEDELAYEDEXPANSION
 
 REM Set variables
 SET "VENV_DIR=.venv"
+SET "PYTON_VERSION=python3.12"
 SET "ORCHESTRATION_SCRIPT=orchestrator.py"
 SET "POETRY_INSTALL_CMD=poetry install"
 
 REM Check if virtual environment exists
 IF NOT EXIST "%VENV_DIR%" (
     echo Creating virtual environment...
-    python -m venv %VENV_DIR%
+    %PYTON_VERSION% -m venv %VENV_DIR%
 )
 
 echo Activate virtual environment
