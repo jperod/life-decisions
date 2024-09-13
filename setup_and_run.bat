ddc204705ea802080098f9d2c83697ef17361875
Pedro <pedro@bubbaleads.com>
2024-09-13 08:12:15 +0200

    add bat

---
+++ setup_and_run.bat
@@ -0,0 +1,26 @@
+@echo off
+SETLOCAL ENABLEDELAYEDEXPANSION
+
+REM Set variables
+SET "VENV_DIR=.venv"
+SET "ORCHESTRATION_SCRIPT=orchestrator.py"
+SET "POETRY_INSTALL_CMD=poetry install"
+
+REM Check if virtual environment exists
+IF NOT EXIST "%VENV_DIR%" (
+    echo Creating virtual environment...
+    python -m venv %VENV_DIR%
+)
+
+echo Activate virtual environment
+CALL %VENV_DIR%\Scripts\activate
+
+echo Install Poetry 
+pip install poetry
+poetry install
+
+echo Running orchestration script...
+python %ORCHESTRATION_SCRIPT%
+
+
+ENDLOCAL
